677cde59e7af6cc8678a55b7f670842c73a82c5d
Saurav Kumar Roy <39926263+sauravslr@users.noreply.github.com>
2021-01-26 14:20:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const autoCompleteConfig = {
         return movie.Title
     },
     async fetchData(searchTerm) {
-    const response = await axios.get('http://www.omdbapi.com/',{
+    const response = await axios.get('https://www.omdbapi.com/',{
         params:{
             apikey: 'd9a7fb0',
             s: searchTerm
